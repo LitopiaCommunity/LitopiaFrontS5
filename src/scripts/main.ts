2de4493b13d9a429003211ca3ea19127e004d3dc
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-08-29 19:18:19 +0200

    Refractor : Separation des composants

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,2 @@
 import '../stylesheets/main.scss'
-
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import './components/test-component'

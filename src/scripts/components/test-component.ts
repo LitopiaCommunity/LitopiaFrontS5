e14c39d17a884d72ef92793a2a39bd97079bee0a
LeChatonSousCoke <71146954+LeChatonSousCoke@users.noreply.github.com>
2021-08-30 12:58:03 +0200

    Ajout de la NavBar

---
+++ src/scripts/components/test-component.ts
@@ -0,0 +1,6 @@
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1 class="hello-vite bg-black">Hello <spam>Vite!</spam></h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
